f3c956e85e19b7772560ab25515180bca795d912
Sébastien Prunier <sebastien.prunier@gmail.com>
2015-01-16 18:54:11 +0100

    Fermeture propre de la connexion MongoDB

--- batch/src/main/java/nosql/workshop/batch/mongodb/CsvToMongoDb.java
+++ batch/src/main/java/nosql/workshop/batch/mongodb/CsvToMongoDb.java
@@ -4,25 +4,33 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
-import java.net.UnknownHostException;
-
 /**
  * Importe les données issues des fichiers CSV dans la base MongoDB.
  */
 public class CsvToMongoDb {
 
-    public static void main(String[] args) throws UnknownHostException {
-        // Création du client Mongo
-        MongoClient mongoClient = new MongoClient();
+    public static void main(String[] args) {
+        MongoClient mongoClient = null;
+        try {
+            // Création du client Mongo
+            mongoClient = new MongoClient();
 
-        // Récupération de la collection "installations" de la base "nosql-workshop"
-        DB db = mongoClient.getDB("nosql-workshop");
-        DBCollection installationsCollection = db.getCollection("installations");
+            // Récupération de la collection "installations" de la base "nosql-workshop"
+            DB db = mongoClient.getDB("nosql-workshop");
+            DBCollection installationsCollection = db.getCollection("installations");
 
-        // Import des données
-        new InstallationsImporter(installationsCollection).run();
-        new EquipementsImporter(installationsCollection).run();
-        new ActivitesImporter(installationsCollection).run();
+            // Import des données
+            new InstallationsImporter(installationsCollection).run();
+            new EquipementsImporter(installationsCollection).run();
+            new ActivitesImporter(installationsCollection).run();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (mongoClient != null) {
+                mongoClient.close();
+            }
+        }
     }
 
 }
