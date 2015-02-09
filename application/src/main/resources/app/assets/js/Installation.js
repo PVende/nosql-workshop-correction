0aaec5a4fcd7c60c5fd74075e5da83745e1c1c19
Sébastien Prunier <sebastien.prunier@gmail.com>
2015-02-10 00:56:00 +0100

    un peu de react.js

---
+++ application/src/main/resources/app/assets/js/Installation.js
@@ -0,0 +1,39 @@
+var Installation = React.createClass({
+    getInitialState: function () {
+        return {};
+    },
+    componentDidMount: function () {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function (data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    render: function () {
+        if (!this.state.data) {
+            return (
+                <div>
+                    <span className="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className="well">
+                    <p>
+                        <b>{'\u0023' + this.state.data._id + ' - ' + this.state.data.nom}</b>
+                    </p>
+                    <p>
+                        <span className="glyphicon glyphicon-map-marker"></span>
+                        {this.state.data.adresse.numero + ' ' + this.state.data.adresse.voie + ' ' + this.state.data.adresse.codePostal + ' ' + this.state.data.adresse.commune}
+                    </p>
+                </div>
+            );
+        }
+    }
+});
