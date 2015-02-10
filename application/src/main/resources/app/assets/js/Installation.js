7de39912cf782d312a5bc7f103f775033eb56fa2
Sébastien Prunier <sebastien.prunier@gmail.com>
2015-02-10 01:07:50 +0100

    affichage des coordonnées GPS de l'installation

--- application/src/main/resources/app/assets/js/Installation.js
+++ application/src/main/resources/app/assets/js/Installation.js
@@ -29,8 +29,12 @@ var Installation = React.createClass({
                         <b>{'\u0023' + this.state.data._id + ' - ' + this.state.data.nom}</b>
                     </p>
                     <p>
-                        <span className="glyphicon glyphicon-map-marker"></span>
+                        <span className="glyphicon glyphicon-road">&nbsp;</span>
                         {this.state.data.adresse.numero + ' ' + this.state.data.adresse.voie + ' ' + this.state.data.adresse.codePostal + ' ' + this.state.data.adresse.commune}
+                    </p>
+                    <p>
+                        <span className="glyphicon glyphicon-map-marker">&nbsp;</span>
+                        {this.state.data.location.coordinates[0] + ' ; ' + this.state.data.location.coordinates[1]}
                     </p>
                 </div>
             );
